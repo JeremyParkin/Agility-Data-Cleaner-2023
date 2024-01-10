b1bb55f91bd801510953f5d45aecb1a6d9ffa23b
Jeremy Parkin <djplana@gmail.com>
2024-01-10 08:56:04 -0500

    escape markdown

--- pages/7-Summaries.py
+++ pages/7-Summaries.py
@@ -194,7 +194,7 @@ else:
     for story in df.index:
 
 
-        head = df.Headline[story] #escape_markdown(df.Headline[story])
+        head = escape_markdown(df.Headline[story])
         outlet = escape_markdown(df["Example Outlet"][story])
         link = escape_markdown(df["Example URL"][story])
         date = df["Date"][story].strftime("%B %d, %Y")
